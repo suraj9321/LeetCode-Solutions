343d295463b3b9791214ff524b77c1d8cd65aead
suraj9321 <suraj9321@gmail.com>
2022-01-21 17:12:51 -0500

    Leetcode Greedy - Maximum Length of Chain Pair

---
+++ src/MaximumLengthOfChainPair.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class MaximumLengthOfChainPair {
+    public int findLongestChain(int[][] A) {
+        Arrays.sort(A,(a, b)->a[1]-b[1]);
+        int maxlen = 1;
+        int end = A[0][1];
+        for(int i=1;i<A.length;i++){
+            if(A[i][0]>end){
+                maxlen++;
+                end = A[i][1];
+            }
+        }
+        return maxlen;
+    }
+}
+
+/*You are given an array of n pairs pairs where pairs[i] = [lefti, righti] and lefti < righti.
+
+A pair p2 = [c, d] follows a pair p1 = [a, b] if b < c. A chain of pairs can be formed in this fashion.
+
+Return the length longest chain which can be formed.
+
+You do not need to use up all the given intervals. You can select pairs in any order.
+
+
+
+Example 1:
+
+Input: pairs = [[1,2],[2,3],[3,4]]
+Output: 2
+Explanation: The longest chain is [1,2] -> [3,4].
+Example 2:
+
+Input: pairs = [[1,2],[7,8],[4,5]]
+Output: 3
+Explanation: The longest chain is [1,2] -> [4,5] -> [7,8].
+
+
+Constraints:
+
+n == pairs.length
+1 <= n <= 1000
+-1000 <= lefti < righti <= 1000*/
